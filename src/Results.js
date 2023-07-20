e6e87c1b5ed328fc367b328082aacb3f9e5619e0
Artur Matysiak <inz.matys@gmail.com>
2023-07-20 14:33:48 +0200

    first commit

---
+++ src/Results.js
@@ -0,0 +1,45 @@
+import "./results.css";
+
+function Results(props) {
+  return (
+    <ul>
+      <li>
+        <h3>
+          Prognoza dla miasta <span>{props.city}</span>
+        </h3>
+      </li>
+      <li>
+        Zachmurzenie: <span> {props.clouds} %</span>
+      </li>
+      <li>
+        Temperatura: <span>{props.temp} *C</span>
+      </li>
+      <li>
+        Wiatr: <span>{props.wind} km/h</span>
+      </li>
+      <li>
+        Wschód słońca: <span>{props.sunrise}</span>
+      </li>
+      <li>
+        Zachód słońca: <span>{props.sunset}</span>
+      </li>
+      <li>
+        <a
+          href="https://stat.gov.pl/badania-statystyczne/sprawozdawczosc/intrastat/alfabetyczny-wykaz-krajow/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kraj:
+        </a>
+        <span> {props.country}</span>
+      </li>
+      <li>
+        <h5>
+          Odczyt z godziny <span>{props.hour}</span>
+        </h5>
+      </li>
+    </ul>
+  );
+}
+
+export default Results;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import Results from "./Results";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Weather extends Component {
+  state = {
+    active: false,
+    hour: 0,
+    city: 0,
+    clouds: 0,
+    temp: 0,
+    wind: 0,
+    sunrise: 0,
+    sunset: 0,
+    country: 0,
+  };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  handleButton = () => {
+    const city = document.getElementById("search_city");
+    this.state.city ? this.getWeather() : alert("wpisz miasto");
+    city.value = "";
+  };
+
+  getCity = () => {
+    const city = document.getElementById("search_city");
+    this.setState({ city: city.value });
+    city.value !== 0 && this.setState({ active: false });
+  };
+
+  getWeather = () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.city},lang=pl&units=metric&appid=1b75f47063fbd0b335b1b1ddb0ee4435`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((results) => {
+        if (results.cod === "404") {
+          results.cod = "404" && alert("nie ma takiego miasta!");
+          this.setState({ active: false });
+        } else {
+          const wTime = results.dt + 7200;
+          const sunrTime = results.sys.sunrise;
+          const sunsTime = results.sys.sunset;
+          const weatherTime = new Date(wTime * 1000).toGMTString();
+          const sunriseTime = new Date(sunrTime * 1000).toLocaleTimeString();
+          const sunsetTime = new Date(sunsTime * 1000).toLocaleTimeString();
+          this.setState({
+            active: true,
+            hour: weatherTime,
+            city: results.name,
+            clouds: results.clouds.all,
+            temp: results.main.temp.toFixed(1),
+            wind: results.wind.speed.toFixed(1),
+            sunrise: sunriseTime,
+            sunset: sunsetTime,
+            country: results.sys.country,
+          });
+        }
+      });
+    this.setState({ active: false });
+  };
+
+  render() {
+    const { hour, city, clouds, temp, wind, sunrise, sunset, country } = this.state;
+    return (
+      <>
+        <h2>Simple Weather (React)</h2>
+        <input type="text" placeholder="wpisz miasto" id="search_city" onChange={this.getCity} />
+        <button onClick={this.handleButton}>Szukaj</button>
+        {this.state.active === true ? (
+          <Results hour={hour} city={city} clouds={clouds} temp={temp} wind={wind} sunrise={sunrise} sunset={sunset} country={country} />
+        ) : null}
+      </>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Weather />);
